f2d10767ed8bde1b222a39ed2e5ee2635948313d
Krutik Hirudkar <krutikhirudkar721@gmail.com>
2026-06-02 22:33:08 +0530

    Update Grocery_shop_bill.py

--- Grocery_shop_bill.py
+++ Grocery_shop_bill.py
@@ -7,6 +7,7 @@ items = {
             3: {"name": "Cooking Oil", "price": 120, "unit": "L"},
             4: {"name": "Milk", "price": 55, "unit": "L"}
         }
+
         cart = {} 
 # Stores {item_id: quantity}
         print("Welcome to Super Bazar!")
